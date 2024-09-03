9528e60019ec3ffbb1aebe9f6f503e452d681b4b
hwang jong wan <https://github.com/hjw1191/game>
2024-09-03 17:38:39 +0900

    처음

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import './global.css'
 import './font.css'
 import './App.css'
-import { Header } from './components/Header'
+import { Header }  from './components/Header'
 import { Footer } from './components/Footer'
 import { Home } from './routes/Home'
 import { Demo } from './routes/Demo';
+import Main  from './routes/Main';
 import { Route, Routes } from 'react-router-dom';
+
 
 
 function App() {
   return (
-    <body className="App">
+    <div className="App">
       <Header/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/Demo" element={<Demo/>}/>
+        <Route path="/Main" element={<Main/>}/>
       </Routes>
       <Footer/>
-    </body>
+    </div>
   ); 
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,28 @@
+import React, { useState } from 'react';
 import './Header.css'
 import { Link } from 'react-router-dom';
-
+import LoginModal from './LoginModal';
+import SignupModal from './SignupModal';
+import ProfileNavbar from './ProfileNavbar';
 
 export function Header() {
+    const [showLoginModal, setShowLoginModal] = useState(false);
+    const [showSignupModal, setShowSignupModal] = useState(false);
+    const [showProfileNavbar, setShowProfileNavbar] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [username, setUsername] = useState('');
+    
+    const handleLogin = (loggedInUsername) => {
+        setIsLoggedIn(true);
+        setUsername(loggedInUsername);
+        setShowLoginModal(false);
+    };
+
+    const handleLogout = () => {
+        setIsLoggedIn(false);
+        setUsername('');
+    };
+
     return (
         <header>
             <Link to="/" id="logo">
@@ -10,14 +30,27 @@ export function Header() {
                 당근마차
             </Link>
             <Link to="/guide">이용가이드</Link>
-            <Link to="/sign">로그인</Link>
-            <Link to="/sign">회원가입</Link>
-            <Link to="/trial">
+            {isLoggedIn ? (
+                <>
+                    <span>안녕하세요, {username}님!</span>
+                    <button onClick={() => setShowProfileNavbar(!showProfileNavbar)}>프로필</button>
+                    <button onClick={handleLogout}>로그아웃</button>
+                </>
+            ) : (
+                <>
+                    <button onClick={() => setShowLoginModal(true)}>로그인</button>
+                    <button onClick={() => setShowSignupModal(true)}>회원가입</button>
+                </>
+            )}
+            <Link to="/main">
                 <div> 
                     <img src="/img/logo.svg" alt="logo" />
                     체험해보기
                 </div>
             </Link>
+            {showLoginModal && <LoginModal onClose={() => setShowLoginModal(false)} onLogin={handleLogin} />}
+            {showSignupModal && <SignupModal onClose={() => setShowSignupModal(false)} />}
+            {showProfileNavbar && <ProfileNavbar onClose={() => setShowProfileNavbar(false)} />}
         </header>
     );
-};  
+};
