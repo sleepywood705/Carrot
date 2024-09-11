63b82c96163009341cae99772125e1c95d3023b6
Dane, Lee <sleepywood705@gmail.com>
2024-09-11 13:28:08 +0900

    마이페이지 디자인

--- fe/src/routes/Mypage2.js
+++ fe/src/routes/Mypage2.js
@@ -1,35 +1,50 @@
+import React, { useState } from "react";
 import "./Mypage2.css";
-import { Routes, Route, Link } from "react-router-dom";
-
 
 export function Mypage2() {
+  const [selectedMenu, setSelectedMenu] = useState(null);
+
+  const renderContent = () => {
+    switch (selectedMenu) {
+      case "changeInfo":
+        return <ChangeInfo />;
+      case "myPoint":
+        return <MyPoint />;
+      case "withdrawal":
+        return <Withdrawal />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <div id="Mypage2">
       <div className="top">
-        <div className="img_profile"></div>
-        <div className="nickname"></div>
+        <div className="cnt_img">
+          <img src="/img/leaf.png" alt="leaf" />
+          <div className="img_profile"></div>
+        </div>
+        <div className="nickname">닉네임</div>
       </div>
       <div className="bot">
         <div id="SNB">
           <details>
             <summary>내 정보 관리</summary>
             <ul>
-              <li>회원정보변경</li>
-              <li>내 포인트</li>
-              <li>회원 탈퇴</li>
+              <li onClick={() => setSelectedMenu("changeInfo")}>회원정보변경</li>
+              <li onClick={() => setSelectedMenu("myPoint")}>내 포인트</li>
+              <li onClick={() => setSelectedMenu("withdrawal")}>회원 탈퇴</li>
             </ul>
           </details>
           <details>
             <summary>이용 관리</summary>
-                <ul>
-                    <li>이용기록</li>
-                </ul>
+            <ul>
+                <li>이용기록</li>
+            </ul>
           </details>
         </div>
         <div>
-          <ChangeInfo />
-          {/* <MyPoint /> */}
-          {/* <Withdrawal /> */}
+          {renderContent()}
         </div>
       </div>
     </div>
@@ -75,7 +90,7 @@ function MyPoint() {
     return (
         <div id="MyPoint">
             <h2>내 포인트</h2>
-            <span>1500</span>
+            <div className="currentPoint">1500 포인트</div>
             <h3>포인트 내역</h3>
             <ul>
                 <li className="pointList">2024-09-10 100포인트 적립</li>
@@ -86,7 +101,6 @@ function MyPoint() {
         </div>
     )
 }
-
 
 
 function Withdrawal() {
