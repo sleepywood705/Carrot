a2c023143a960200b0e6e65675caa62d691f5f1d
Dane, Lee <sleepywood705@gmail.com>
2024-09-22 17:50:00 +0900

    랜딩페이지 (미완)

--- fe/src/components/Nav.js
+++ fe/src/components/Nav.js
@@ -8,7 +8,6 @@ export function Nav({ onClose, userName }) {
 			<button className="close-button" onClick={onClose}>&times;</button>
 			<h2>{userName}님의 프로필</h2>
 			<Link to='/mypage'>마이페이지</Link>
-			<Link to='/settings'>설정</Link>
 			<Link to='/messages'>메시지</Link>
 			<Link to='/history'>거래 내역</Link>
 		</nav>
